c31a2d56cf33e0efb5501d45447b10c01e47aa4f
Amara <Amara.com.hp@gmail.com>
2026-09-17 14:14:15 -0400

    complated task 1 and 2

---
+++ script.js
@@ -0,0 +1,33 @@
+// Task #1
+function formatFullName(firstName,lastName){
+  
+
+  if (!firstName || !lastName) {
+    return "Invalid name input.";
+}
+
+
+firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+
+  return lastName + ", " + firstName;
+}
+
+console.log(formatFullName("amara", "guifarro"));
+
+// Task #2
+
+function calculateTotalCost(price, quantity, taxRate) {
+
+  totalCost = (price * quantity) * (1 + taxRate);
+
+  return totalCost;
+
+}
+
+  console.log(calculateTotalCost(20, 4, 0.08));
+
+
+
+
+
