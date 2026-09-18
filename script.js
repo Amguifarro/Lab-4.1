43fc3a06eb8eecfe8c65aad946e6d10b03e228a3
Amara <Amara.com.hp@gmail.com>
2026-09-17 21:52:17 -0400

    Completed task#3

--- script.js
+++ script.js
@@ -25,7 +25,36 @@ function calculateTotalCost(price, quantity, taxRate) {
 
 }
 
-  console.log(calculateTotalCost(20, 4, 0.08));
+  console.log(calculateTotalCost( 15, 4, 0.08));
+
+  // Task #3
+  function checkEligibility(age, isEmployed) {
+
+    if (age > 18 && isEmployed) {
+
+      return "Eligible";
+  
+    }
+
+    else if(age > 18 && isEmployed == false) {
+
+      return "Conditionally Eligible";
+    }
+
+    else {
+      return ("Not eligible")
+    }
+
+  }
+
+  console.log(checkEligibility(20, true));
+  console.log(checkEligibility(20, false));
+  console.log(checkEligibility(17, true));
+  console.log(checkEligibility(18, true));
+
+   
+
+  
 
 
 
