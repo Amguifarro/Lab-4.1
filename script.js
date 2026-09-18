c57fc86ff5066bc2ac2c361342952a1c46d29d8e
Amara <Amara.com.hp@gmail.com>
2026-09-17 22:13:30 -0400

    Added discount function to task 2

--- script.js
+++ script.js
@@ -17,15 +17,24 @@ console.log(formatFullName("amara", "guifarro"));
 
 // Task #2
 
-function calculateTotalCost(price, quantity, taxRate) {
+function calculateTotalCost(price, quantity, taxRate, discount = 0) {
 
-  totalCost = (price * quantity) * (1 + taxRate);
+  
+    let subtotal = price * quantity;
 
-  return totalCost;
+    subtotal = subtotal - discount;
+    
+    let totalCost = subtotal * (1 + taxRate);
 
-}
+    return totalCost;
+  }
+
+
+
 
   console.log(calculateTotalCost(20, 4, 0.08));
+
+  console.log(calculateTotalCost(20, 4, 0.08, 5))
 
 
 
